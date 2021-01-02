1eb046360d713e4fc9b752a253275636e670ddb8
ingeechart <ingeechart@gmail.com>
2021-01-02 17:09:01 +0900

    renew namings of parameters

--- config.py
+++ config.py
@@ -1,16 +1,19 @@
 import os
+import yacs
 from yacs.config import CfgNode as CN
 
 _C = CN()
 
-_C.EXP_NAME = 'testing'
-_C.OUTPUT_DIR = 'result/'
-_C.LOG_DIR = 'result/looog.log'
-_C.GPUS = (0,1,2,4)
-_C.WORKERS = 8
-_C.PRINT_FREQ = 20
-_C.PIN_MEMORY = True
-_C.LOCAL_RANK = ''
+''' Define system environment for Training '''
+_C.SYS = CN()
+_C.SYS.EXP_NAME = 'testing'
+_C.SYS.OUTPUT_DIR = 'results/' + _C.SYS.EXP_NAME + '/'
+
+_C.SYS.GPUS = (0,1,2,4)
+_C.SYS.WORKERS = 8
+_C.SYS.PIN_MEMORY = True
+_C.SYS.LOCAL_RANK = ''
+
 # Cudnn related params
 _C.CUDNN = CN()
 _C.CUDNN.BENCHMARK = True
@@ -20,12 +23,19 @@ _C.CUDNN.ENABLED = True
 
 _C.TRAIN = CN()
 _C.TRAIN.RESUME = ''
-_C.TRAIN.INIT_LR = 0.1
-_C.TRAIN.MOMENTUM = 0.9
-_C.TRAIN.WD = 5e-4
-_C.TRAIN.START_EPOCH = 0
+_C.TRAIN.START_EPOCH = 1
 _C.TRAIN.END_EPOCH = 400
+_C.TRAIN.PRINT_FREQ = 20
+_C.TRAIN.BATCH_SIAE = 16
+_C.TRAIN.BATCH_SIAE_VAL = 4
+_C.TRAIN.SEED = 1007
 
+_C.TRAIN.OPT = CN()
+_C.TRAIN.OPT.NAME = 'SGD'
+_C.TRAIN.OPT.LR = 0.02
+_C.TRAIN.OPT.NBB_LR = 0.06
+_C.TRAIN.OPT.WD = 0.0001
+_C.TRAIN.OPT.MOMENTUM = 0.9
 
 ''' common params for NETWORK '''
 _C.MODEL = CN()
@@ -33,20 +43,25 @@ _C.MODEL.NAME = 'model'
 _C.MODEL.BB_PRETRAINED = ''
 _C.MODEL.LOSS = ''
 _C.MODEL.FREEZE_BN = ''
-_C.MODEL.NONBACKBONE_KEYWORDS = []
-_C.MODEL.NONBACKBONE_MULT = 10
+_C.MODEL.NBB_KEYWORDS = []
+
  
 
-
 # DATASET related params
-_C.DATASET = CN()
-_C.DATASET.ROOT = ''
-_C.DATASET.DATASET = 'cityscapes'
-_C.DATASET.NUM_CLASSES = 19
-_C.DATASET.TRAIN_SET = ''
-_C.DATASET.TRAIN_BATCH_SIZE = 16
-_C.DATASET.VAL_BATCH_SIZE = 4
-_C.DATASET.TEST_SET = ''
+_C.DATA = CN()
+_C.DATA.ROOT = 'cityscapes/'
+_C.DATA.SET = 'cityscapes'
+_C.DATA.NUM_CLASSES = 19
+_C.DATA.TRAIN_SPLIT = 'train'
+_C.DATA.VAL_SPLIT = 'val'
+_C.DATA.TEST_SPLIT = 'test'
+_C.DATA.SHUFFLE = False
+_C.DATA.DROP_LAST = True
+
+_C.DATA.AUG = CN()
+_C.DATA.AUG.R_SCALE = [0.5,2]
+_C.DATA.AUG.CROP_SIZE = [768, 1536] # [1024,2048]
+
 
 def get_cfg_defaults():
   """Get a yacs CfgNode object with default values for my_project."""
@@ -56,7 +71,13 @@ def get_cfg_defaults():
 
 
 if __name__ == '__main__':
-    import sys
-    with open(sys.argv[1], 'w') as f:
-      print(sys.argv)
-      print(_C, file=f)
+    # import sys
+    # with open(sys.argv[1], 'w') as f:
+    #   print(sys.argv)
+    #   print(_C, file=f)
+    cfg = get_cfg_defaults()
+    lists = ['TRAIN.START_EPOCH', 100, 'TRAIN.BEST_ACC', 100]
+    print(lists)
+    cfg.merge_from_list(lists)
+    # cfg.TRAIN.BEST_MIOU = 100
+    print(cfg)
--- train.py
+++ train.py
@@ -15,11 +15,12 @@ import torch.backends.cudnn as cudnn
 from torch.nn.parallel import DistributedDataParallel
 import torch.distributed as dist
 import torch.optim as optim
+import torch.nn.functional as F
 
 
 from model import build_model
-from data import build_train_loader, build_val_loader
-from utils.utils import update_config, get_logger, adjust_learning_rate, AverageMeter, intersectionAndUnionGPU
+from data.buildDataloader import build_train_loader, build_val_loader
+from utils.utils import *
 
 if torch.__version__ <= '1.1.0':
     from tensorboardX import SummaryWriter
@@ -41,7 +42,7 @@ def parse_args():
     args = parser.parse_args()
 
     cfg = config.get_cfg_defaults()
-    cfg = update_config(cfg, args)
+    cfg = merge_config(cfg, args)
 
     return args, cfg
 
@@ -50,28 +51,29 @@ def main():
     args, cfg = parse_args()
 
     # *  define paths ( output, logger) * #
-    if not os.path.exists(cfg.OUTPUT_DIR):
-        os.makedirs(cfg.OUTPUT_DIR)
+    if not os.path.exists(cfg.SYS.OUTPUT_DIR):
+        os.makedirs(cfg.SYS.OUTPUT_DIR)
         
     timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
-
-    logger_path = 'result/'+cfg.EXP_NAME+'_'+timestamp +'.log'
+    logger_path = cfg.SYS.OUTPUT_DIR+'log/'+timestamp +'.log'
 
     global logger, tWriter, vWriter
     logger = get_logger(logger_path)
     tWriter = SummaryWriter(cfg.OUTPUT_DIR+'tb_data/train')
     vWriter = SummaryWriter(cfg.OUTPUT_DIR+'tb_data/val')
     
-    # # * controll random seed * #
-    # random.seed(cfg.RANDOM_SEED)
-    # torch.manual_seed(cfg.RANDOM_SEED)
+    # * controll random seed * #
+    torch.manual_seed(cfg.TRAIN.SEED)
+    torch.cuda.manual_seed(cfg.TRAIN.SEED)
+    torch.cuda.manual_seed_all(cfg.TRAIN.SEED) # if use multi-GPU
+    np.random.seed(cfg.TRAIN.SEED)
+    random.seed(cfg.TRAIN.SEED)
 
 
     # TODO: findout what cudnn options are
     cudnn.benchmark = cfg.CUDNN.BENCHMARK
     cudnn.deterministic = cfg.CUDNN.DETERMINISTIC
     cudnn.enabled = cfg.CUDNN.ENABLED
-
 
 
     msg = '[{time}]' 'starts experiments setting '\
@@ -114,10 +116,33 @@ def main():
 
     # * define OPTIMIZER * #
     params_dict = dict(model.named_parameters())
-    params = [{'params': list(params_dict.values()), 'lr': cfg.TRAIN.INIT_LR}]
-    # params = [{'params': bb_lr, 'lr': init_lr}, {'params': nbb_lr, 'lr': init_lr*10}]    
-    optimizer = torch.optim.SGD(params, lr=cfg.TRAIN.INIT_LR, momentum=cfg.TRAIN.MOMENTUM, weight_decay=cfg.TRAIN.WD)
+    if cfg.cfg.MODEL.NBB_KEYWORDS:
+        bb_lr = []
+        nbb_lr = []
+        nbb_keys = set()
+        for k, param in params_dict.items():
+            if any(part in k for part in cfg.MODEL.NBB_KEYWORDS):
+                nbb_lr.append(param)
+                nbb_keys.add(k)
+            else:
+                bb_lr.append(param)
+        print(nbb_keys)
+        params = [{'params': bb_lr, 'lr': cfg.TRAIN.OPT.LR}, 
+                {'params': nbb_lr, 'lr': cfg.TRAIN.OPT.NBB_LR}]
+    else:
+        params = [{'params': list(params_dict.values()), 'lr': cfg.TRAIN.OPT.LR}]
 
+    optimizer = torch.optim.SGD(params, lr=cfg.TRAIN.OPT.LR, 
+                                        momentum=cfg.TRAIN.OPT.MOMENTUM, 
+                                        weight_decay=cfg.TRAIN.OPT.WD)
+
+    # * build DATALODER * #
+    train_loader = build_train_loader(cfg)
+    val_loader = build_val_loader(cfg)
+
+    # * Training setup * #
+    best_mIoU = 0
+    best_epoch = 0
 
     # * RESUME * #
     if cfg.TRAIN.RESUME:
@@ -127,8 +152,13 @@ def main():
 
             checkpoint = torch.load(cfg.TRAIN.RESUME, map_location=lambda storage, loc: storage.cuda())
             
-            start_epoch = checkpoint['epoch']
+            new_epoch = ['TRAIN.START_EPOCH' ,checkpoint['epoch']]
+            cfg = update_config(cfg, new_epoch)
 
+            best_mIoU = checkpoint['best_mIoU']
+            best_epoch = checkpoint['best_epoch']
+
+            # state_dict control
             checkpoint_dict = checkpoint['state_dict']
             model_dict = model.state_dict()
             checkpoint_dict = {'module.'+k: v for k, v in checkpoint_dict.items()
@@ -143,7 +173,9 @@ def main():
             model_dict.update(checkpoint_dict)
             model.load_state_dict(model_dict, strict=True)
 
+            # optimizer control
             optimizer.load_state_dict(checkpoint['optimizer'])
+
             if dist.get_rank()==0:
                 logger.info("=> loaded checkpoint '{}' (epoch {})".format(cfg.TRAIN.RESUME, checkpoint['epoch']))
         else:
@@ -151,49 +183,39 @@ def main():
                 logger.info("=> no checkpoint found at '{}'".format(cfg.TRAIN.RESUME))
 
 
-
-    # * build DATALODER * #
-    train_loader = build_train_loader(cfg)
-    val_loader = build_val_loader(cfg)
-
-
-    best_mIoU = 0
-    best_epoch = 0
-
     logger.info('starts training')
-    for epoch in range(cfg.TRAIN.START_EPOCH, cfg.TRAIN.END_EPOCH):
-        epoch_log = epoch+1
-        loss_train, mIoU_train, mAcc_train, allAcc_train = train(model, train_loader, optimizer, epoch, cfg)
+    for epoch in range(start_epoch, cfg.TRAIN.END_EPOCH):
 
-        if args.local_rank <= 0:
-            tWriter.add_scalar('loss', loss_train, epoch_log)
-            tWriter.add_scalar('mIoU', mIoU_train, epoch_log)
-            tWriter.add_scalar('mAcc', mAcc_train, epoch_log)
-            tWriter.add_scalar('allAcc', allAcc_train, epoch_log)
-            torch.save({
-                'epoch': epoch_log,
-                'state_dict': model.module.state_dict(),
-                'optimizer': optimizer.state_dict(),
-            }, os.path.join(cfg.OUTPUT_DIR,'checkpoint.pth.tar'))
-        
+        loss_train, mIoU_train, mAcc_train, allAcc_train = train(model, train_loader, optimizer, epoch, cfg)
         loss_val, mIoU_val, mAcc_val, allAcc_val = validation(model, val_loader, cfg)
 
-
         if args.local_rank <= 0:
-            vWriter.add_scalar('loss', loss_val, epoch_log)
-            vWriter.add_scalar('mIoU', mIoU_val, epoch_log)
-            vWriter.add_scalar('mAcc', mAcc_val, epoch_log)
-            vWriter.add_scalar('allAcc', allAcc_val, epoch_log)
+            tWriter.add_scalar('loss', loss_train, epoch)
+            tWriter.add_scalar('mIoU', mIoU_train, epoch)
+            tWriter.add_scalar('mAcc', mAcc_train, epoch)
+            tWriter.add_scalar('allAcc', allAcc_train, epoch)
+
+            vWriter.add_scalar('loss', loss_val, epoch)
+            vWriter.add_scalar('mIoU', mIoU_val, epoch)
+            vWriter.add_scalar('mAcc', mAcc_val, epoch)
+            vWriter.add_scalar('allAcc', allAcc_val, epoch)
         
-            ## TODO 이 부분 한번 확인해야함. check point for best validation
             if best_mIoU < mIoU_val:
                 torch.save({
-                    'epoch': epoch+1,
+                    'epoch': epoch,
                     'state_dict': model.module.state_dict(),
                     'optimizer': optimizer.state_dict(),
                 }, os.path.join(cfg.OUTPUT_DIR,'best.pth.tar'))
                 best_mIoU = mIoU_val
-                best_epoch = epoch+1
+                best_epoch = epoch
+
+            torch.save({
+                'epoch': epoch,
+                'state_dict': model.module.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'best_mIoU': best_mIoU,
+                'bset_epoch': best_epoch,
+            }, os.path.join(cfg.OUTPUT_DIR,'checkpoint.pth.tar'))
 
         if args.local_rank <= 0:
             msg = 'Loss_train: {:.10f}  Loss_val: {:.10f}'.format(loss_train, loss_val)
@@ -219,8 +241,8 @@ def train(model, train_loader, optimizer, epoch, cfg):
     target_meter = AverageMeter('Target')
 
     model.train()
-    max_iter = cfg.TRAIN.END_EPOCH * len(train_loader)
     end = time.time()
+    max_iter = cfg.TRAIN.END_EPOCH * len(train_loader)
 
     for i_iter, (imgs, target) in enumerate(train_loader):
         data_time.update(time.time() - end)
@@ -241,13 +263,12 @@ def train(model, train_loader, optimizer, epoch, cfg):
         if dist.is_initialized():
             loss = loss * n
             dist.all_reduce(loss)
-            n = 2*dist.get_world_size()
+            n = n*dist.get_world_size()
             loss = loss / n
         loss_meter.update(loss.item(), n)
 
 
         # TODO 코드 확인. (evaluation metric), need to be changed with eval code of cityscape dataset
-        #* 이부분이 정말 필요한가...? 연산도 이상하게 했다...
         intersection, union, target = intersectionAndUnionGPU(predict, target, 19, 255)
         if dist.is_initialized():
             dist.all_reduce(intersection), dist.all_reduce(union), dist.all_reduce(target)
@@ -263,7 +284,8 @@ def train(model, train_loader, optimizer, epoch, cfg):
         current_iter = epoch * len(train_loader) + i_iter + 1
 
         lr = adjust_learning_rate(optimizer,
-                                  cfg.TRAIN.INIT_LR,
+                                  cfg.TRAIN.OPT.LR,
+                                  cfg.TRAIN.OPT.NBB_LR
                                   max_iter,
                                   current_iter)
 
@@ -275,19 +297,21 @@ def train(model, train_loader, optimizer, epoch, cfg):
         remain_time = '{:02d}:{:02d}:{:02d}'.format(int(t_h), int(t_m), int(t_s))
         
         if (i_iter+1) % cfg.PRINT_FREQ == 0 and dist.get_rank() == 0:
-            msg ='Epoch: [{}/{}][{}/{}] '\
+            msg ='Epoch: [{}/{}]({:.2f}%) [{}/{}] '\
                     'Data {data_time.val:.3f} ({data_time.avg:.3f}) '\
                     'Batch {batch_time.val:.3f} ({batch_time.avg:.3f}) '\
                     'Remain {remain_time} '\
                     'Loss {loss_meter.val:.4f} '\
                     'Accuracy {accuracy:.4f} '\
-                    'lr {lr}.'.format(epoch+1, cfg.TRAIN.END_EPOCH, i_iter + 1, len(train_loader),
-                                                        batch_time=batch_time,
-                                                        data_time=data_time,
-                                                        remain_time=remain_time,
-                                                        loss_meter=loss_meter,
-                                                        accuracy=accuracy,
-                                                        lr = [x['lr'] for x in optimizer.param_groups])
+                    'lr {lr}.'.format(epoch+1, cfg.TRAIN.END_EPOCH, 
+                                    ((epoch+1)/cfg.TRAIN.END_EPOCH)*100, 
+                                    i_iter + 1, len(train_loader),
+                                    batch_time=batch_time,
+                                    data_time=data_time,
+                                    remain_time=remain_time,
+                                    loss_meter=loss_meter,
+                                    accuracy=accuracy,
+                                    lr = [x['lr'] for x in optimizer.param_groups])
             logger.info(msg)
 
         if dist.get_rank() == 0:
@@ -304,7 +328,9 @@ def train(model, train_loader, optimizer, epoch, cfg):
     mAcc = np.mean(accuracy_class)
     allAcc = sum(intersection_meter.sum) / (sum(target_meter.sum) + 1e-10)
     if dist.get_rank() == 0:
-        logger.info('Train result at epoch [{}/{}]: mIoU/mAcc/allAcc {:.4f}/{:.4f}/{:.4f}.'.format(epoch+1, cfg.TRAIN.END_EPOCH, mIoU, mAcc, allAcc)) # max epoch: 200
+        logger.info('Train result at epoch [{}/{}]: mIoU/mAcc/allAcc {:.4f}/{:.4f}/{:.4f}.'.format(
+                            epoch+1, cfg.TRAIN.END_EPOCH, mIoU, mAcc, allAcc))
+
     return loss_meter.avg, mIoU, mAcc, allAcc
 
 
@@ -332,7 +358,7 @@ def validation(model, val_loader, cfg):
             if dist.is_initialized():
                 loss = loss * n
                 dist.all_reduce(loss)
-                n = 2*dist.get_world_size()
+                n = n*dist.get_world_size()
                 loss = loss / n
             else:
                 loss = torch.mean(loss)
